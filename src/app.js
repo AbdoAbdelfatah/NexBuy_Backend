648a7f123d6a99c719150bbcc0110d0b57974e76
Bahaa Zenhom <bahaazenhom7@gmail.com>
2025-10-23 19:44:52 +0300

    Remove CORS middleware from backend

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import userRoutes from "./routers/user.router.js";
 import productRoutes from "./routers/product.router.js";
 import commentRoutes from "./routers/comment.router.js";
@@ -8,30 +7,7 @@ import errorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
 
-// CORS configuration
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      process.env.FRONTEND_URL,
-      'http://localhost:4200',
-      'https://nexbuy-frontend.obl.ee'
-    ].filter(Boolean); // Remove undefined values
-    
-    // Allow requests with no origin (like mobile apps, Postman, etc.)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-
 // global middlewares
-app.use(cors(corsOptions));
 app.use(express.json());
 
 // register routes
